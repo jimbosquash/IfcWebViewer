938c624ad92268bb7276dbefc060453827ae2d90
James Hayward <hhaywardj@gmail.com>
2024-08-05 15:49:00 +0200

    change file name

--- src/scenes/global/AppRouter.tsx
+++ src/scenes/global/AppRouter.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import { ThreeScene } from "../viewer/threeScene";
-import TopBar from "../global/TopBar";
+import TopDisplay from "../global/TopDisplay";
 import TopBarContextProvider from "../../context/TopBarContext";
 
 export const AppRouter = () => {
@@ -9,7 +9,7 @@ export const AppRouter = () => {
     <BrowserRouter>
     <TopBarContextProvider>
       <div style={{ display: "flex",flexDirection: "column",height: "100%", width: "100vw" }}>
-        <TopBar/>
+        <TopDisplay/>
         <main style={{ flex: 1, padding: "0",display: "flex", overflow: "hidden" }}>
           <Routes>
             {/* <Route path='/' element={<LandingPage/>} /> */}
