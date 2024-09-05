bf3cfabc5c9da387656759d6cc12ba0c4dbf9ac5
James Hayward <hhaywardj@gmail.com>
2024-09-05 12:06:28 +0200

    feat: tree box has get color switch

--- src/components/TreeTableRow.tsx
+++ src/components/TreeTableRow.tsx
@@ -1,6 +1,6 @@
 import { Icon } from "@iconify/react";
 import { Box, IconButton, Tooltip, Typography, useTheme } from "@mui/material";
-import { useState, useEffect, ReactElement } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { ModelCache } from "../bim-components/modelCache";
 import { useComponentsContext } from "../context/ComponentsContext";
 import { nonSelectableTextStyle } from "../styles";
@@ -24,6 +24,8 @@ export interface TreeTableRowProps {
   setEnabled: (args: string, enabled: boolean) => void;
   children?: React.ReactNode; // Add this line to support children
   variant: "Floating" | "Flat";
+//   parentHovered: boolean;
+//   parentSelect: boolean;
 }
 
 export const TreeTableRow: React.FC<TreeTableRowProps> = ({
@@ -35,13 +37,15 @@ export const TreeTableRow: React.FC<TreeTableRowProps> = ({
   setEnabled,
   variant,
   children,
+//   parentHovered = false,
+//   parentSelect = false,
 }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const components = useComponentsContext();
   const [isHovered, setIsHovered] = useState(false);
   const [isSelected, setIsSelected] = useState(false);
-  // const [isVisible, setIsVisible] = useState<boolean>(isEnabled); // check this
+  const [isExpanded, setIsExpanded] = useState<boolean>(false); // check this
 
   // on double click select 3d geometry
   const handelDoubleClick = () => {
@@ -104,6 +108,21 @@ export const TreeTableRow: React.FC<TreeTableRowProps> = ({
     // console.log("hover clear", highlighter?.selection["hover"]);
   }, [isHovered]);
 
+  
+  const getColor = useCallback((element: 'text' | 'background') => {
+    if (!isEnabled && !isHovered) {
+      // Not visible state
+      return element === 'text' ? colors.grey[800] : colors.primary[400];
+    }
+    if (isHovered) {
+      // Hovered state
+      return element === 'text' ? colors.primary[100] : colors.blueAccent[600];
+    }
+    // Normal state
+    return element === 'background' ? colors.grey[1000] : colors.grey[500];
+  }, [isEnabled, isHovered, colors]);
+
+
   const floatingBoxTheme = {
     boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
     padding: isHovered ? "8px" : "10px",
@@ -117,14 +136,14 @@ export const TreeTableRow: React.FC<TreeTableRowProps> = ({
     borderColor: colors.grey[1000],
     // border: isSelected ? "1.5px solid #ccc" : "0.8px solid #ccc",
     border: "0.8px solid #ccc",
-    backgroundColor: isHovered ? colors.grey[800] : colors.grey[1000],
-    transition: "all 0.2s ease",
+    backgroundColor: getColor('background'),
+    transition: "all 0.1s ease",
     justifyContent: "space-between",
     overflow: "hidden", // Ensures no overflow issues
   };
   const flatBoxTheme = {
     // boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
-    padding: isHovered ? "8px" : "0px",
+    padding: "0px",
     width: "100%",
     height: "30px",
     margin: "0px 0",
@@ -136,19 +155,20 @@ export const TreeTableRow: React.FC<TreeTableRowProps> = ({
     // border: isSelected ? "1.5px solid #ccc" : "0.8px solid #ccc",
     borderBottom: "0.8px solid #ccc",
     borderTop: "0.8px solid #ccc",
-    backgroundColor: isHovered ? colors.grey[800] : colors.grey[1000],
-    transition: "all 0.2s ease",
+    backgroundColor: getColor('background'),
+    transition: "all 0.02s ease",
     justifyContent: "space-between",
     overflow: "hidden", // Ensures no overflow issues
   };
 
   return (
-    <Tooltip title={name} arrow placement="right">
+    // <Tooltip title={name} arrow placement="right">
       <Box
         component="div"
         sx={{
           width: "100%",
           height: "100%", // Ensure it takes full height of parent
+          minHeight:'30px',
           display: "flex",
           flexDirection: "column",
           overflow: "hidden", // Hide overflow at this level
@@ -170,8 +190,22 @@ export const TreeTableRow: React.FC<TreeTableRowProps> = ({
             minWidth: 0, // Allows flex items to shrink below their minimum content size
           }}
         >
+            <IconButton
+            size="small"
+            sx={{
+              flexShrink: 0, // Prevent button from shrinking
+              marginLeft: "8px",
+              color: getColor('text'),
+            }}
+            onClick={(e: any) => {
+              e.stopPropagation();
+              setEnabled(node?.id ?? "", !isEnabled);
+            }}
+          >
+            {isEnabled ? <Icon style={{fontSize:15}} icon="eva:radio-button-on-fill" />: <Icon style={{fontSize:15}} icon="eva:radio-button-off-outline" />}
+          </IconButton>
           {icon && (
-            <Icon icon={icon} style={{ flexShrink: 0 }} color={isHovered ? colors.primary[100] : colors.grey[500]} />
+            <Icon icon={icon} style={{ flexShrink: 0, marginLeft:"5px" }} color={getColor('text')} />
           )}
           <Typography
             noWrap
@@ -182,7 +216,7 @@ export const TreeTableRow: React.FC<TreeTableRowProps> = ({
               flexGrow: 1,
               flexShrink: 1,
               minWidth: 0, // Allows text to shrink
-              color: isHovered ? colors.primary[100] : colors.grey[600],
+              color: getColor('text'),
               ...nonSelectableTextStyle,
               ml: 1,
               variant: isSelected ? "body2" : "body1",
@@ -201,32 +235,34 @@ export const TreeTableRow: React.FC<TreeTableRowProps> = ({
               flexShrink: 1,
               minWidth: 0, // Allows text to shrink
               marginLeft: "10px",
-              color: isHovered ? colors.primary[100] : colors.grey[600],
+              color: getColor('text'),
               variant: "body2",
               whiteSpace: "nowrap",
               overflow: "hidden",
+              marginRight: children ? '0px' : '30px',
               textOverflow: "ellipsis",
               display: { xs: "none", sm: "block" }, // Responsive: hides on extra small screens
             }}
           >
             Qnt : {node?.children?.size ?? ""}
           </Typography>
-          <IconButton
-            size="small"
-            sx={{
-              flexShrink: 0, // Prevent button from shrinking
-              marginLeft: "8px",
-              color: isHovered ? colors.primary[100] : colors.grey[500],
-            }}
-            onClick={(e: any) => {
-              e.stopPropagation();
-              setEnabled(node?.id ?? "", !isEnabled);
-            }}
-          >
-            {isEnabled ? <Icon icon="mdi:checkbox-outline" /> : <Icon icon="mdi:checkbox-blank-outline" />}
-          </IconButton>
+          
+        {children && <IconButton
+        size="small"
+        sx={{
+            flexShrink: 0, // Prevent button from shrinking
+            marginLeft: "8px",
+            color: getColor('text'),
+        }}
+        onClick={(e: any) => {
+            e.stopPropagation();
+            setIsExpanded(!isExpanded);
+        }}
+        >
+        {isExpanded ? <Icon icon="system-uicons:minus" /> : <Icon icon="system-uicons:plus" />}
+        </IconButton>}
         </Box>
-        {children && (
+        { isExpanded && children !== undefined && (
           <Box
             component="div"
             sx={{
@@ -241,7 +277,7 @@ export const TreeTableRow: React.FC<TreeTableRowProps> = ({
           </Box>
         )}
       </Box>
-    </Tooltip>
+    // </Tooltip>
   );
 };
 
