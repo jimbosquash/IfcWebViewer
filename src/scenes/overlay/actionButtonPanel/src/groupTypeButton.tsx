0f4516f8184971247d66b00936ff3a854b4708ce
James Hayward <hhaywardj@gmail.com>
2024-09-12 11:37:57 +0200

    feat: change selection group button working

--- src/scenes/overlay/actionButtonPanel/src/groupTypeButton.tsx
+++ src/scenes/overlay/actionButtonPanel/src/groupTypeButton.tsx
@@ -1,8 +1,11 @@
 import { Icon } from "@iconify/react";
 import { Button, ToggleButtonGroup, Tooltip, useTheme } from "@mui/material";
 import { useState } from "react";
+import { ModelViewManager } from "../../../../bim-components/modelViewer";
 import { useComponentsContext } from "../../../../context/ComponentsContext";
 import { tokens } from "../../../../theme";
+import { TreeNode, TreeUtils } from "../../../../utilities/Tree";
+import { BuildingElement, SelectionGroup } from "../../../../utilities/types";
 
 export const GroupTypeButton = () => {
   const theme = useTheme();
@@ -11,7 +14,56 @@ export const GroupTypeButton = () => {
   const [groupType, setGroupType] = useState<string>();
 
   const toggleButtonGroup = () => {};
+
+  const toggleGroupType = () => {
+    //get current selected node
+
+    // if child is not type building element then set its first child as the selected group
+
+    const viewManager = components.get(ModelViewManager);
+    const current = viewManager.SelectedGroup;
+    if (!current) {
+      //should select first group 
+      return;
+    }
+    console.log("group type", current.groupType);
+    const node = viewManager.Tree?.getNode(current.id);
+    if(!node) {
+      console.log("failed to change group type due to node not being found in primary tree",current)
+      return;
+    }
+
+    if(node.isLeaf) {
+      // get parent instead
+      return;
+    }
+
+    const children = [...node.children.entries()];
+    let newNode = children.find((value) => value[1].type !== "BuildingElement")?.[1];
+    if(!newNode && node.parent?.type !== "Project") {
+      newNode = node.parent;
+
+    }
+    if(newNode) {
+      console.log("current group will change from",node.type," to:", newNode.type)
+
+      viewManager.setSelectionGroup(NodeToSelectionGroup(newNode),true)}
+
+  };
+
+  const NodeToSelectionGroup = (node : TreeNode<BuildingElement>) => {
+
+    const group: SelectionGroup = {
+      id: node.id,
+      groupType: node.type,
+      groupName: node.name,
+      elements: TreeUtils.getChildrenNonNullData(node)
+    }
+    return group;
+  }
+
   // const toggleGroupType = () => {
+
   //   const viewManager = components.get(ModelViewManager);
   //   const current = viewManager.SelectedGroup;
   //   if(!current) return;
@@ -46,7 +98,7 @@ export const GroupTypeButton = () => {
   return (
     <>
       <Tooltip title={"Navigate Building Steps"}>
-        <Button onClick={toggleButtonGroup} style={{ color: colors.grey[200], border: "0" }} variant={"outlined"}>
+        <Button onClick={toggleGroupType} style={{ color: colors.grey[200], border: "0" }} variant={"outlined"}>
           {groupType === "Assembly" ? <Icon icon="solar:box-outline" /> : <Icon icon="ri:shapes-line" />}
         </Button>
       </Tooltip>
